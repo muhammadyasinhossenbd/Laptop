026296a8640c56a092856b77c54838f84e4ffa8e
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-11 17:39:40 +0600

    Module 14.01 Watched

---
+++ JS-Conditionals/aunty.js
@@ -0,0 +1,15 @@
+/**
+ * Comparison
+ * 1. More than: >
+ * 2. Lees than: <
+ * 3. Equal: ==
+ * 4. Greater Than or Equal: >=
+ * 5. Less than or Equal: <=
+ * 6. More than or Equal: <=
+ * 7. Not Equal: !=
+ * 
+ * --------------
+ * 
+ * 8. and: &&
+ * 9. or: || 
+ */
--- JS-Conditionals/variables.js
+++ JS-Conditionals/variables.js
@@ -1,2 +1,32 @@
 // VARIABLES ===> Container
 
+// Number Type
+var sunglassPrice=80;
+
+// String
+var subject = "EEE";
+
+// Boolean
+var passed = true;
+
+
+
+/**
+ *Naming Convention
+ 1. Single Word
+ 2. No Quote
+ 3. no gap or dash
+ 4. can not start with a number
+ 5. can not be a keyword
+ 6. prefer camleCase
+*/
+
+var lastExamScore = 89;
+/**
+ * Operarion
+ * 1. +, -, *, /, %
+ * 2. +=. -=. *=, /=
+ * 3. isNan?
+ * 4. null
+ * 5. Number 
+ */
