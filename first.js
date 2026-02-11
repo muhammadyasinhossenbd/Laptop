8f927eb68c311f5e56335ac7638bc25efcbd787e
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-11 11:37:27 +0600

    module 13.8 watched

--- first.js
+++ first.js
@@ -1,1 +1,15 @@
-console.log(22)
+var orangeprice = 20;
+var chocolateprice = 0.5;
+// var appleprice = parseInt("50");
+var appleprice = parseFloat("50");
+
+console.log(orangeprice + appleprice);
+
+
+// var first = 0.1;
+// var second = 0.2;
+// var total = first + second;
+// console.log(total.toFixed(2))
+
+// const num = -50/0;
+// console.log(num)
