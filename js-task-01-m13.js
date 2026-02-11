c417bee94124e96102c33a31fa77b3b34d076063
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-11 13:55:42 +0600

    Practice Task Completed

---
+++ js-task-01-m13.js
@@ -0,0 +1,47 @@
+// Task 01 
+
+// var capital = 1000;
+// var orangePrice = 300;
+// var applePrice =350;
+// var tPrice = orangePrice + applePrice;
+// capital = capital -tPrice;
+
+// console.log(capital)
+
+
+// Task 02
+
+// Subjects
+
+// var Mathematics = parseFloat(78.5);
+// var Biology = parseFloat(90.50);
+// var Chemistry = 65;
+// var Physics = 80;
+// var Bangla = parseFloat(85.90);
+
+// var subT= Mathematics + Biology + Chemistry + Physics + Bangla;
+// console.log(subT)
+
+// var average = Number ((subT / 5).toFixed(2));
+
+// console.log("Average:", average)
+// // console.log(typeof average)
+
+
+// Task 03
+
+
+// Remainder Output
+
+
+// var x = 119;
+// var y = 5;
+// var modu = x % y;
+// console.log(modu)
+
+// var a = isNaN("11");
+var a = isNaN(2-10);
+
+console.log(a);
+
+
