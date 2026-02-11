f6f4abe58d6132357aa965c61e1217c58d1a11e3
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-11 19:40:26 +0600

    If Else Watched

---
+++ JS-Conditionals/if.js
@@ -0,0 +1,33 @@
+/**
+ * -------------Structure--------
+ * if (condition){
+ * 
+ * }
+ * ----------------------------------------------------
+ * if condition{
+ *      execute the code if the condition is true
+
+* }
+ * else{
+ *      execute the code if the condition is false
+ * }
+ * ----------------------------------------------------------
+ */
+
+// if (13 < 10){
+//     console.log("I Have a Smaller Value")
+// }
+
+// var biriyaniPrice = 200;
+// if (biriyaniPrice < 300){
+//     console.log("Mama Give me Some Biriyani")
+// }
+var weight = 35;
+if (weight <= 20 ){
+    console.log( "I Will Carry Myself")
+}
+else {
+console.log("Will rent a Rickshaw")
+}
+
+
