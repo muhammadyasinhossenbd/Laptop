f0ec287e987c4d12ab0732b9bfe28dcec29a439a
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-13 01:49:27 +0600

    Push-Pop

---
+++ JS-Conditionals/array.js
@@ -0,0 +1,4 @@
+const numbers = [1 , 2, 3, 4, 5, 6, 7]
+const friends = ["Abul" , "Hasan", "Salim", "Mehedi"]
+const passed = [true, false, false, true]
+const mixedArray = ["abul", 32, true, 45.65]
---
+++ JS-Conditionals/push-pop.js
@@ -0,0 +1,9 @@
+const numbers = [1 , 2, 3, 4, 5, 6, 7]
+// console.log(numbers);
+numbers.push(32);
+console.log(numbers);
+numbers.pop()
+console.log(numbers);
+if (numbers.includes(2)){
+    console.log("party")
+}
