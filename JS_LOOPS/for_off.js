ce0691ebaf1d3fa0f515dcf0a6a054d14e7c0646
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-13 02:32:57 +0600

    For Off Added

---
+++ JS_LOOPS/for_off.js
@@ -0,0 +1,15 @@
+const numbers = [ 12, 98, 63, 43, 21, 72];
+console.log(numbers);
+for (const num of numbers){
+    console.log(num)
+}
+
+const fruits = ['orange', 'apple', 'malta', 'kola']
+// for (){
+ 
+// }
+
+for (const fruit of fruits){
+ console.log(fruit)
+ console.log('I want to Gift my Wife')
+}
---
+++ JS_LOOPS/loop.js
@@ -0,0 +1,16 @@
+// const : Value Change hobe na 
+// let : Value can change
+// var : We'll not use it
+
+// 
+const amount = 200;
+
+if (amount > 100){
+
+}
+else {
+
+}
+
+// array
+const numbers = [12, 14, 16, 18, 20];
