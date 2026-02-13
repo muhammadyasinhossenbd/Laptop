54f70d8851aa775c298ef283717d8947368e726a
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-14 00:37:58 +0600

    Loop

---
+++ JS_LOOPS/for.js
@@ -0,0 +1,30 @@
+// let num = 0;  //loop Variable
+// while (num <= 5){ //Loop Condition
+//     console.log(num) //Loop Statement
+//     num++; // Loop Variable Change
+// }
+
+/***
+ * for (   ;    ;   ){
+ * }
+ * 
+ * for (first part; second part; third part){
+ * Loop Statement}
+ * 
+ * for (Loop Variable declaration; Loop Condition; Loop variable Change){
+ * Loop Statement}
+ * 
+ * 
+ * 
+ * 
+ */
+
+let num = 0;  //loop Variable
+// while (num <= 5){ //Loop Condition
+//     console.log(num) //Loop Statement
+//     num++; // Loop Variable Change
+// }
+
+for (let num = 0; num <= 5; num++){
+    console.log(num);
+}
---
+++ JS_LOOPS/while.js
@@ -0,0 +1,10 @@
+// -------------- WHILE_LOOP_______________
+let num = 0;
+while(num 
+    < 10 ){
+console.log('Loooooooping', num)
+// num = num + 1
+// num += 1
+num ++
+
+}
