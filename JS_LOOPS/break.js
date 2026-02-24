6b108328560007e4d77f2cc0b74d2d994484b2cc
muhammadyasinhossenbd <muhammadyasinhossenbd@gmail.com>
2026-02-24 23:08:25 +0600

    Break & Continue Video watched

---
+++ JS_LOOPS/break.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <105; i++){
+    console.log(i)
+    if(i >=50){
+        break;
+    }
+}
+console.log('Life after breakup');
---
+++ JS_LOOPS/continue.js
@@ -0,0 +1,20 @@
+// continue ---> Skip this rest of the code for this itoration 
+// break ------> i'm done with this loop. loop end
+
+// for (let i=1; i<=10; i++){
+//     if(i%2===1){
+//         continue;
+//             }
+//             console.log(i)
+// }
+
+let n = 0;
+while (n < 50){
+    n++;
+    if (n%15 !== 0){
+        continue;
+        }
+    console.log(n)
+            
+
+}
